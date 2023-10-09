10a1922a81bae95f4477b3f6b73d2fa3f3fc7359
Ali Gutierrez <70215619+aligutierrez@users.noreply.github.com>
2023-10-08 23:51:03 -0600

    Update Alphabetizable.swift

--- Sources/AlphabetScrollBar/Alphabetizable.swift
+++ Sources/AlphabetScrollBar/Alphabetizable.swift
@@ -3,6 +3,6 @@ import Foundation
 
 // Protocol for items that can be sorted alphabetically.
 public protocol Alphabetizable: Identifiable {
-  var Id: String { get }
+  var  alphabetizableId: String { get }
   var alphabetizableName: String { get }
 }
